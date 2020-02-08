a59d6a1fa588b4e6efba4ae9d42e9a9567c8042b
Jenny Cooke <jcooke1986uk@users.noreply.github.com>
2020-02-08 15:24:03 +0000

    Made `formatObject` recursive.

--- src/js/keys.js
+++ src/js/keys.js
@@ -1,7 +1,19 @@
-export const formatObject = (keys, data) => Object.entries(data).reduce((obj, [key, value]) => ({
-  ...obj,
-  [keys[key] || key]: value,
-}), {});
+export const formatObject = (keys, data) => Object.entries(data).reduce((obj, [key, value]) => {
+  let response;
+
+  if (Array.isArray(value) && typeof value[0] === 'object') {
+    response = formatObjectArray(keys, value);
+  } else if (typeof value === 'object') {
+    response = formatObject(keys, value);
+  } else {
+    response = value;
+  }
+  
+  return {
+    ...obj,
+    [keys[key] || key]: response
+  }
+}, {});
 
 export const formatObjectArray = (keys, data) => data.map((entry) => formatObject(keys, entry));
 
