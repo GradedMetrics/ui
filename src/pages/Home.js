d19edd38a4c7c43b5a07ad0ba2152cebcce15fa1
jcooke1986uk <jcooke.web@gmail.com>
2020-02-21 16:36:11 +0000

    Adding background image and a temporary link on the home page to the sets page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { paths } from 'js/routes';
 
 function Home() {
   return (
-    <h2>Home</h2>
+    <>
+      <h2>Home</h2>
+      <Link to={paths.sets}>
+        Sets
+      </Link>
+    </>
   );
 }
 
